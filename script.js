22bd7d7881778054ebbf2ae60191440aca77f5ce
MENÉNDEZ GUERRA <67031016+MENENDEZGUERRA@users.noreply.github.com>
2023-08-26 15:46:12 -0600

    Finished functional ui

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Returns the computer choice
 function getComputerChoice(){
     let computerChoiceSeed = Math.floor(Math.random() * 3) + 1;
     let computerChoiceText;
@@ -17,51 +18,87 @@ function getComputerChoice(){
     return computerChoiceText;
 }
 
+let userScore = 0;
+let computerScore = 0;
 
+//return the result of the game using the user choice
 function playRound(userChoice) {
     let computerSelection = getComputerChoice();
+
+
     console.log("The computer selection is: " + computerSelection);
     if(computerSelection == "rock" && userChoice == "paper"){
+        userScore += 1;
         return "You Won!";
     }else if (computerSelection == "paper" && userChoice == "rock"){
+        computerScore += 1;
         return "You Lose!";
     }else if (computerSelection == "scissors" && userChoice == "rock"){
+        userScore += 1;
         return "You Won!";
     }else if (computerSelection == "rock" && userChoice == "scissors"){
+        computerScore += 1;
         return "You Lose!";
     }else if(computerSelection == "scissors" && userChoice == "paper"){
+        computerScore += 1;
         return "You Lose!";
     }else if(computerSelection == "rock" && userChoice == "paper"){
+        userScore += 1;
         return "You Won!";
     }else if (computerSelection == "paper" && userChoice == "scissors"){
+        userScore += 1;
         return "You Won!";
     }
     else if (computerSelection == userChoice) {
         return "Draw";
     }
     else {
+        computerScore += 1;
         return "Invalid choice, so you lose";
     }
 }
 
-function game(){
-    let userScore = 0;
-    let computerScore = 0;
 
-    const computerSelection = getComputerChoice().toLowerCase();
-    let userChoice = prompt("Enter your choice (Rock, Paper Scissors): ").toLowerCase();
-    let result = playRound(computerSelection, userChoice);
-    console.log("The computer selection is " + computerSelection + " " + result)
+
+//display the results
+function scoreCount(computerScore, userScore) {
+    const cs = document.getElementById('computer-score');
+    cs.innerHTML = "Computer Score: " + computerScore;
+
+    const us = document.getElementById('user-score');
+    us.innerHTML = "Human Score: " + userScore;
+
+    if (userScore = 5){
         
-    if (result == "You Lose!"){
-        computerScore += 1;
-    }else if (result == "You Won!"){
-        userScore += 1;
-    }
 
-    console.log("Computer Score: " + computerScore + " User Score: " + userScore)
+
+    }else if (computerScore >= 5){
+        const wn = document.getElementById('winner');
+        wn.innerHTML = "The winner is: Computer";
+
+    }
 }
 
+//buttons
 function reply_click(clicked_id){
     console.log(playRound(clicked_id));
+    if (computerScore === 5){
+
+        const wn = document.getElementById('winner');
+        wn.innerHTML = "The winner is: Computer";
+        userScore = 0;
+        computerScore = 0;
+        scoreCount(computerScore, userScore);
+    } else if (userScore === 5) {
+        
+        const wn = document.getElementById('winner');
+        wn.innerHTML = "The winner is: User";
+        userScore = 0;
+        computerScore = 0;
+        scoreCount(computerScore, userScore);
+
+    } else {
+        scoreCount(computerScore, userScore);
+    }
+
 }
